fd7b3ae0f92fff0d62b0d58ce0d0158970254ef2
Sahrin Tahiyat Choudhury <144997115+Sahrin-Tahiyat-Choudhury@users.noreply.github.com>
2023-10-10 00:47:20 +0530

    Q-2.C

---
+++ GCSC-Taskade-Solutions/Semester-1/Q-2.C
@@ -0,0 +1,11 @@
+//Write a program to print ASCII Value of a given character.
+
+
+#include<stdio.h>
+void main()
+{
+    char a;
+    printf("Enter a character");
+    scanf("%c",&a);
+    printf("ASCII value of %c is %d",a,a);
+}
